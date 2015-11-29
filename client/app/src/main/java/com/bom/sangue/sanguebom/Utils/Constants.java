d363edc3730d20133cfd7391bc4ae32a2c3c2849
alan <alan_araujo_sousa@hotmail.com>
2015-11-29 15:33:48 -0200

    [CLIENT] Correction on register a new user.

--- client/app/src/main/java/com/bom/sangue/sanguebom/Utils/Constants.java
+++ client/app/src/main/java/com/bom/sangue/sanguebom/Utils/Constants.java
@@ -5,7 +5,12 @@ package com.bom.sangue.sanguebom.Utils;
  */
 public class Constants {
 
-    public static final String HOST = "10.0.2.2";
+    // Use this for ADB
+//    public static final String HOST = "10.0.2.2";
+
+    // Use this for Genymotion
+    public static final String HOST = "10.0.3.2";
+
     public static final String PORT = "8000";
     public static final String URL_SIGNUP = "http://" + HOST+":"+PORT+"/engine/users/";
     public static final String URL_SIGN = "http://"+HOST+":"+PORT+"/api-auth-token/";
