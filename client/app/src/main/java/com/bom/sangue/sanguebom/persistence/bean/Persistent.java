990824ce68f346be7b8284f17322c6b49c31e25b
Alan Araujo <alan@usto.re>
2019-09-01 22:42:19 -0300

    separate andorid client repository of django server

--- client/app/src/main/java/com/bom/sangue/sanguebom/persistence/bean/Persistent.java
+++
@@ -1,9 +0,0 @@
-package com.bom.sangue.sanguebom.persistence.bean;
-
-/**
- * Created by alan on 25/11/15.
- */
-public interface Persistent {
-    Long getId();
-    void setId(Long id);
-}
--- server/engine/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from engine.models import Donation, UserProfile, Patient
-
-admin.site.register(Donation)
-admin.site.register(UserProfile)
-admin.site.register(Patient)
